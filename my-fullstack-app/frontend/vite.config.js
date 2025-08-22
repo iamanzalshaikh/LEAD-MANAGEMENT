a69284c7e717f966dab380127bae951d2587c457
iamanzalshaikh <tocxanzalftw49@gmail.com>
2025-08-22 20:52:04 +0530

    Update vite.config.js

--- my-fullstack-app/frontend/vite.config.js
+++ my-fullstack-app/frontend/vite.config.js
@@ -1,3 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins: [react()] });
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    fs: {
+      allow: ['.']
+    }
+  },
+  base: '/', // make sure base path is '/'
+});
